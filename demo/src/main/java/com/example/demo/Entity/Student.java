0e8aab325afee2502992f3bdcd7c90c69cc37fbb
effk <effk@host.docker.internal>
2023-02-02 14:42:42 +0800

    Entity modify

--- demo/src/main/java/com/example/demo/Entity/Student.java
+++ demo/src/main/java/com/example/demo/Entity/Student.java
@@ -1,5 +1,19 @@
 package com.example.demo.Entity;
 
 public class Student {
-
+	public int ID;
+	public String name;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
